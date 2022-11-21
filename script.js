38ffc557f1cfa081bf9d6a0bd14220aeb9692517
Edmund Debrah <edmundking05@gmail.com>
2022-11-21 16:28:31 +0000

    add computation functions

---
+++ script.js
@@ -0,0 +1,24 @@
+function add (a, b) {
+    let sum = a + b;
+    return sum;
+}
+
+function subtract (a, b) {
+    let subtractedResult = a - b;
+    return subtractedResult;
+}
+
+function multiply (a, b) {
+    let multiple = a * b;
+    return multiple;
+}
+
+function divide (a, b) {
+    let divident = a / b;
+    return divident;
+
+}
+
+function operate (a, b, operation) {
+
+}
